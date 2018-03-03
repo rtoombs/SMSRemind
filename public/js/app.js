aca26dbea0db9b3cc403452ba63061431f700f0a
ridid44 <ridid44@DESKTOP-CJHMFEQ>
2018-03-03 17:24:09 -0600

    Added ability to update list item by double clicking. Identified and handled issue where if two list items have the same value updaing one will update both.

--- public/js/app.js
+++ public/js/app.js
@@ -4,25 +4,55 @@ function friend(name) {
     }
 }
 
-var viewModel = function(){
+var vm = function () {
     var self = this;
-    this.firstName = ko.observable("Riley");
-    this.lastName = ko.observable("Toombs");
-    this.friends = ko.observableArray([new friend("Steve"), new friend("Tom"),new friend("Frank"),new friend("John")]);
+    this.friends = ko.observableArray([new friend("Steve"), new friend("Frank"), new friend("Frank"), new friend("John")]);
 
-    this.addFriend = function() {
-        console.log(this);
+    this.addFriend = function () {
         this.friends.push(new friend("Poop"));
+        console.log(this);
     };
 
-    this.deleteItem = function(y) {
+    this.deleteItem = function (y) {
         self.friends.remove(y);
-        console.log(self.friends());
+    };
+
+    this.updateItem = function (oldItem, newItem, pos) {
+        for (var item of this.friends()) {
+            if (item.name() == oldItem && this.friends().indexOf(item) == parseInt(pos)) {
+                self.friends.replace(item, new friend(newItem));
+                break;
+            }
+        }
+        console.log(this.friends());
     };
 }
 
+$(document).ready(function () {
+    var ViewModel = new vm();
+    ko.applyBindings(ViewModel);
+    $("#sortable").sortable();
 
-$( document ).ready(function() {
-    ko.applyBindings(new viewModel());
-    $( "#sortable" ).sortable();
+    var origVal;
+    $(".todoList").on('dblclick', '.todoText', function () {
+        origVal = $(this).text();
+        $(this).text("");
+        $("<input type='text'>").appendTo(this).focus();
+    });
+
+    $(".todoList").on('focusout', '.todoText > input', function () {
+        var $this = $(this);
+        var newVal = $this.val().trim();
+        var index = $this.parent().attr("index");
+        //$this.parent().text($this.val() || origVal);
+        if (newVal.length) {
+            $this.parent().text(newVal);
+            ViewModel.updateItem(origVal, newVal, index);
+        }
+        else {
+            $this.parent().text(origVal);
+        }
+        $this.remove();
+    });
+
 });
